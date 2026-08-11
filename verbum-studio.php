dfb1bc60728e69c1c66683beb393e0f86b109537
soniaaleandrade-alt <soniaaleandrade@gmail.com>
2026-08-11 13:23:24 -0300

    Set plugin version to 2.5.3

--- verbum-studio.php
+++ verbum-studio.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Verbum Studio
  * Description: Core foundation for the Verbum Studio writing operating system.
- * Version: 2.5.2
+ * Version: 2.5.3
  * Author: Verbum Studio
  * Requires PHP: 7.4
  */
@@ -16,7 +16,7 @@ if (version_compare(PHP_VERSION, '7.4', '<')) {
     return;
 }
 
-define('VERBUM_STUDIO_VERSION', '2.5.2');
+define('VERBUM_STUDIO_VERSION', '2.5.3');
 define('VERBUM_STUDIO_FILE', __FILE__);
 define('VERBUM_STUDIO_PATH', plugin_dir_path(__FILE__));
 define('VERBUM_STUDIO_URL', plugin_dir_url(__FILE__));
