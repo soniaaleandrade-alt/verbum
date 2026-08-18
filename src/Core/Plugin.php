e116a5a5ea31cafd0afd2df50001ef98a65c30d9
soniaaleandrade-alt <soniaaleandrade@gmail.com>
2026-08-18 12:50:26 -0300

    HOM-028: registrar serviços Carta e Alma

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace VerbumStudio\Core;
 
 use VerbumStudio\Api\AuthController;
+use VerbumStudio\Api\FoundationLetterSoulController;
 use VerbumStudio\Api\IdentificationInitialController;
 use VerbumStudio\Api\LibraryController;
 use VerbumStudio\Api\ResponseFactory;
@@ -27,6 +28,7 @@ use VerbumStudio\Auth\Capabilities;
 use VerbumStudio\Integrations\Elementor\ElementorIntegration;
 use VerbumStudio\Integrations\Supabase\SupabaseClient;
 use VerbumStudio\Integrations\Supabase\SupabaseConfig;
+use VerbumStudio\Library\FoundationLetterSoulRepository;
 use VerbumStudio\Library\LibraryPostTypes;
 use VerbumStudio\Library\LibraryRepository;
 use VerbumStudio\Library\WorkAuditRepository;
@@ -61,7 +63,7 @@ final class Plugin
     public function register(): void
     {
         foreach ([
-            RestController::class, AuthController::class, LibraryController::class, IdentificationInitialController::class, WorkProjectController::class,
+            RestController::class, AuthController::class, LibraryController::class, IdentificationInitialController::class, FoundationLetterSoulController::class, WorkProjectController::class,
             WorkPlanningController::class, WorkDevelopmentController::class, WorkChapterPreparationController::class,
             WorkChapterResearchController::class, WorkChapterWritingController::class, WorkChapterRevisionController::class,
             WorkGeneralReviewController::class, WorkVersionsController::class, WorkAuditController::class,
@@ -105,6 +107,7 @@ final class Plugin
         $this->container->set(AuthController::class, static fn (Container $c): AuthController => new AuthController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class)));
         $this->container->set(LibraryPostTypes::class, static fn (): LibraryPostTypes => new LibraryPostTypes());
         $this->container->set(LibraryRepository::class, static fn (): LibraryRepository => new LibraryRepository());
+        $this->container->set(FoundationLetterSoulRepository::class, static fn (): FoundationLetterSoulRepository => new FoundationLetterSoulRepository());
         $this->container->set(WorkProjectRepository::class, static fn (): WorkProjectRepository => new WorkProjectRepository());
         $this->container->set(WorkPlanningRepository::class, static fn (): WorkPlanningRepository => new WorkPlanningRepository());
         $this->container->set(WorkDevelopmentRepository::class, static fn (): WorkDevelopmentRepository => new WorkDevelopmentRepository());
@@ -122,6 +125,7 @@ final class Plugin
 
         $this->container->set(LibraryController::class, static fn (Container $c): LibraryController => new LibraryController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class)));
         $this->container->set(IdentificationInitialController::class, static fn (Container $c): IdentificationInitialController => new IdentificationInitialController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class)));
+        $this->container->set(FoundationLetterSoulController::class, static fn (Container $c): FoundationLetterSoulController => new FoundationLetterSoulController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(FoundationLetterSoulRepository::class)));
         $this->container->set(WorkProjectController::class, static fn (Container $c): WorkProjectController => new WorkProjectController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(WorkProjectRepository::class)));
         $this->container->set(WorkPlanningController::class, static fn (Container $c): WorkPlanningController => new WorkPlanningController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(WorkPlanningRepository::class)));
         $this->container->set(WorkDevelopmentController::class, static fn (Container $c): WorkDevelopmentController => new WorkDevelopmentController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(WorkDevelopmentRepository::class)));
@@ -137,8 +141,8 @@ final class Plugin
         $this->container->set(WorkLegalController::class, static fn (Container $c): WorkLegalController => new WorkLegalController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(WorkLegalRepository::class)));
         $this->container->set(WorkPublicationController::class, static fn (Container $c): WorkPublicationController => new WorkPublicationController($c->get(Config::class), $c->get(ResponseFactory::class), $c->get(Capabilities::class), $c->get(LibraryRepository::class), $c->get(WorkPublicationRepository::class)));
         $this->container->set(FrontendAssets::class, static fn (): FrontendAssets => new FrontendAssets());
-        $this->container->set(SupabaseConfig::class, static fn (Container $c): SupabaseConfig => new SupabaseConfig($c->get(Config::class)));
-        $this->container->set(SupabaseClient::class, static fn (Container $c): SupabaseClient => new SupabaseClient($c->get(SupabaseConfig::class)));
+        $this->container->set(SupabaseConfig::class, static fn (Container $c) => new SupabaseConfig($c->get(Config::class)));
+        $this->container->set(SupabaseClient::class, static fn (Container $c) => new SupabaseClient($c->get(SupabaseConfig::class)));
         $this->container->set(ElementorIntegration::class, static fn (): ElementorIntegration => new ElementorIntegration());
     }
 }
